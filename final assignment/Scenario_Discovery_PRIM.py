19b30e228919060c520b36c248c86237c9db9d23
tessahuysinga <tessahuysinga@hotmail.com>
2025-06-12 13:37:59 +0200

    Nieuwe PRIM aanpassingen

--- final assignment/Scenario_Discovery_PRIM.py
+++ final assignment/Scenario_Discovery_PRIM.py
@@ -19,17 +19,53 @@ def get_do_nothing_dict():
 
 if __name__ == "__main__":
     model, _ = get_model_for_problem_formulation(4)
+    scenarios=100
+    policies=4
     with MultiprocessingEvaluator(model, n_processes=-1) as evaluator:
-        results = evaluator.perform_experiments(scenarios=100, policies=[Policy("baseline", **get_do_nothing_dict())] )
+        results = evaluator.perform_experiments(scenarios=scenarios, policies=policies )
 
         experiments, outcomes = results
 
-        #
-        # hri_ok   = outcomes["Hydrological Resilience Index"][:, :-1, :].min(axis=(1,2)) >= 0
-        # ead_ok   = outcomes["Expected Annual Damage"].sum(axis=1).max(axis=1) < 5e6
-        # rfr_ok   = outcomes["RfR Total Costs"].flatten() < 50e6   # shape (n_exp,)
-        #
-        # y = hri_ok & ead_ok & rfr_ok
-        #
-        # cols_unc           = [c for c in experiments.columns if "_Bmax" in c or "_Brate" in c]
-        # print (cols_unc)
+        print(experiments)
+
+        # ---- 2.1 Hydrological Resilience Index ----
+        hri_sys = outcomes["Hydrological Resilience Index"][:, -1, :].mean(axis=1)  # (N,) systeembreed
+        target_hri = hri_sys < 0
+
+        # ---- 2.2 Expected Annual Damage ----
+        ead_total = outcomes["Expected Annual Damage"].sum(axis=1)  # (N,)
+        ead_q75 = np.percentile(ead_total, 75)
+        target_ead = ead_total >= ead_q75
+
+        # ---- 2.3 RfR Total Costs ----
+        rfr_costs = outcomes["RfR Total Costs"].sum(axis=1)  # (N,)
+        rfr_q75 = np.percentile(rfr_costs, 75)
+        target_rfr = rfr_costs >= rfr_q75
+
+        # ---- 2.4 Combineer tot één worst-case target ----
+        y = target_hri | target_ead | target_rfr        # booleaanse vector (N,)
+        print(f"worst-cases: {y.sum()} van {scenarios*policies} runs  ({y.mean()*100:.1f} %)")
+
+        #Determine model uncertainties
+        cols_unc = [u.name for u in model.uncertainties]
+        X = experiments[cols_unc]
+
+        #Start PRIM
+        prim_alg = prim.Prim(
+            X, y,
+            threshold=0.8,
+            peel_alpha=0.1,
+            mass_min=0.05
+        )
+
+        box = prim_alg.find_box()
+
+        box.show_tradeoff()
+        plt.show()
+
+        box.inspect()
+
+
+
+
+
